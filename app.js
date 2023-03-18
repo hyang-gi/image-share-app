7f25e283567ff5195b2777cdc15b8a2c37a2e794
Nikhat Mohsin <nikhatmohsin22@gmail.com>
2023-03-18 19:12:44 +0000

    restructuring

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./views"));
 app.use(express.static(path.join(__dirname, "./assets")));
 
+app.get("/", (req, res) => {
+    return res.render("templates") });
+
 
 app.get("/hi/:personName/:personLastName", (req, res) => {
     const name = req.params.personName;
