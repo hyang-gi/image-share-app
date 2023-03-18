db113cd9016b46a9be0fdc1a287fc1e79153ed16
Nikhat Mohsin <nikhatmohsin22@gmail.com>
2023-03-18 17:13:21 +0000

    added structure and basics to repo

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const path = require("path");
+const ejs = require("ejs");
 const PORT = 5000;
 
-const app = express();
+let app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./views"));
+app.use(express.static(path.join(__dirname, "./assets")));
 
-app.get("/", (req, res) => {
-res.send("Welcome!");
-})
+
+app.get("/hi/:personName/:personLastName", (req, res) => {
+    const name = req.params.personName;
+    const lastName = req.params.personLastName;
+    return res.render("templates", { data: { name, lastName } });
+  });
 
 app.listen(PORT, () => {
     console.log(`Server listening on Port: ${PORT}`);
-})
+});
 
