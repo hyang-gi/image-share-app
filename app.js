167cb611642c2f6d76b8f6c17b8737c7bed1cc1f
Nikhat Mohsin <nikhatmohsin22@gmail.com>
2023-04-15 15:51:46 +0100

    added uploaded date to view post

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.get("/", async (req, res) => {
     try {
         const [results] = await dbConnection.query('SELECT * FROM vw_image_interaction_summaries ORDER BY image_uploaded_on DESC');
         console.log("fetched images go here", results);
-        if(req.isAuthenticated()) {
+        if (req.isAuthenticated()) {
             const getUsername = req.user.username;
             const [all_liked_interactions] = await dbConnection.query(
                 `SELECT *
@@ -113,8 +113,8 @@ app.get("/", async (req, res) => {
                  LEFT JOIN interactions ON vw_image_interaction_summaries.image_display_id = interactions.interaction_img_id
                  WHERE interactions.interaction_by = ? AND interactions.interaction_type = '1'`,
                 [getUsername]);
-    
-             authenticated_images = results.map(image => {
+
+            authenticated_images = results.map(image => {
                 const liked = all_liked_interactions.find(interaction => interaction.image_display_id === image.image_display_id);
                 return {
                     ...image,
@@ -351,6 +351,9 @@ app.get("/posts/:post_id", checkAuthenticated, async (req, res) => {
         console.log("image results", img_results);
         const liked_by_user = interactions.find(interaction => interaction.interaction_type === 1 && interaction.interaction_by === username) !== undefined;
         console.log("interaction results", interactions);
+        const timestamp = img_results[0].image_uploaded_on;
+        const date = new Date(timestamp);
+        const uploaded_on = date.toDateString();
         return res.render("templates/index.ejs", {
             page: "../pages/viewPost.ejs",
             title: "View Post",
@@ -360,7 +363,8 @@ app.get("/posts/:post_id", checkAuthenticated, async (req, res) => {
             isUsersPage: false,
             image: img_results[0],
             interactions: interactions,
-            liked_by_user
+            liked_by_user,
+            uploaded_on
         });
     } catch (err) {
         console.log(err);
